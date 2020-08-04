903f770719784d26512dec0ad64ffe0d3d8922d2
RickyStyvani <61553073+Ricky-Styvani@users.noreply.github.com>
2020-08-04 09:27:21 +0700

    membuat header,todoform,todos component

---
+++ src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+    </section>
+  );
+};
+
+export default Todos;
