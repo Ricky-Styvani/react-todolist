75d55479008e523022b2905edcf278c2f76f2551
RickyStyvani <61553073+Ricky-Styvani@users.noreply.github.com>
2020-08-04 14:32:47 +0700

    merefaktor todos dan todolist component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,33 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Todo from "./Todo";
-const Todos = () => {
-  const todos = [
-    {
-      text: "Belajar React"
-    },
-    {
-      text: "Belajar Html"
-    },
-    {
-      text: "Belajar Props dengan react"
-    },
-    {
-      text: "Belajar svkvnsnvnvls"
-    },
-    {
-      text: "Belajar bjsbvskbvkvn"
-    },
-    {
-      text: "Belajar bsjbjb"
-    },
-    {
-      text: "Belajar bvvv"
-    },
-    {
-      text: "Belajar uhvjj"
-    }
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map(todo => {
@@ -35,6 +10,14 @@ const Todos = () => {
       })}
     </section>
   );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  )
 };
 
 export default Todos;
