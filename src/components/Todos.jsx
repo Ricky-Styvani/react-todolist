290cf9337e694a09f474da22042f36a7eeed2b86
RickyStyvani <61553073+Ricky-Styvani@users.noreply.github.com>
2020-08-04 10:22:32 +0700

    membuat todolist page dan todo components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 
+import Todo from "./Todo";
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar Html"
+    },
+    {
+      text: "Belajar Props dengan react"
+    },
+    {
+      text: "Belajar svkvnsnvnvls"
+    },
+    {
+      text: "Belajar bjsbvskbvkvn"
+    },
+    {
+      text: "Belajar bsjbjb"
+    },
+    {
+      text: "Belajar bvvv"
+    },
+    {
+      text: "Belajar uhvjj"
+    }
+  ];
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map(todo => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
