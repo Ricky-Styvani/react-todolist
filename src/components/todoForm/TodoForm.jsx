33db4bd37a696e81a61c1d49070d6c74977a4566
RickyStyvani <61553073+Ricky-Styvani@users.noreply.github.com>
2020-08-12 10:28:12 +0700

    membuat style todos dengn css module

---
+++ src/components/todoForm/TodoForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./todoForm.module.css";
+
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No Blank Todo");
+      return;
+    }
+
+    if (value.length > 40) {
+      alert("Please create a shorter todo text!");
+      setValue("");
+      return;
+    }
+    addTodo(value);
+    setValue("");
+  };
+
+  if (showAdd) {
+    return (
+      <section className={styles.add}>
+        <form action="" className={styles.addForm} onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className={styles.addInput}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button className={styles.addBtn}>Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.IsRequired,
+  showAdd: PropTypes.bool.IsRequired
+};
+
+export default TodoForm;
